1d73aae739ead997643043f2b4f715f06c34c923
Carminelm <carminelm54@gmail.com>
2024-06-18 16:06:36 +0200

    add member list

---
+++ main.js
@@ -0,0 +1,29 @@
+//Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+
+const teamList = [
+    {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    image: 'angela-caroll-chief-editor.jpg'
+},
+{
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    image: 'walter-gordon-office-manager.jpg'
+},
+{
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    image: 'angela-lopez-social-media-manager.jpg'
+},
+{
+    name: 'Scott Estrada',
+    role: 'Developer',
+    image: 'scott-estrada-developer.jpg'
+},
+{
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    image: 'barbara-ramos-graphic-designer.jpg'
+}
+]
